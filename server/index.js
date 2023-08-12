2a3b787828ca95e8d9e23b299be2930e20baaf61
Mehmet Yıldız <myildizwork@gmail.com>
2023-08-13 02:18:29 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://calculation-tool-client.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URL, {
