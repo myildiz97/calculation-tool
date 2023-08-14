0a9702330bdf7fc6ebc2f78a26db260b5c2867cd
Mehmet Yıldız <myildizwork@gmail.com>
2023-08-14 12:27:03 +0300

    Update MainApp.jsx

--- client/src/pages/MainApp.jsx
+++ client/src/pages/MainApp.jsx
@@ -6,6 +6,11 @@ const MainApp = () => {
   // const baseUrlImg = "https://calculation-tool.vercel.app";
 
   const [lastPage, setLastPage] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleNext = () => setCurrentPage(prevPage => prevPage + 1);
+
+  const handlePrev = () => setCurrentPage(prevPage => prevPage - 1);
 
   useEffect(() => {
     axios.get("/app")
@@ -16,7 +21,55 @@ const MainApp = () => {
   return (
     <div className="app-wrapper">
       {lastPage?.error && <p>No page configured yet...</p>}
-      
+      <>
+        {
+          lastPage && lastPage.image.map((page, index) => (
+            <div key={index} style={{ display: index === currentPage ? 'block' : 'none' }}>
+              <div className="app-img">
+                <img src={baseUrlImg + lastPage?.image[index]} width={60} />
+              </div>
+              <div className="app-info">
+                <div className="app-info-header">
+                  <h1>{lastPage?.title[index]}</h1>
+                  <h3>{lastPage?.description[index]}</h3>
+                </div>
+                  { index !== lastPage.image.length - 1 ? (
+                    <div className="app-info-inputs">
+                      {
+                        lastPage?.variableName[index][0].split(",").map((name, i) => (
+                          <input
+                            key={"input-" + i}
+                            type="number"
+                            id={name}
+                            placeholder={lastPage?.placeholder[index][0].split(",")[i]}
+                          />
+                        ))
+                      }
+                    </div>
+                    ): (
+                      <div className="app-info-output">
+                        {
+                          lastPage?.outputValue.map((value, i) => (
+                            <input
+                              key={"output-" + i}
+                              type="number"
+                              id={value}
+                              placeholder={lastPage?.outputName[i]}
+                            />
+                          ))
+                        }
+                      </div>
+                    )
+                  }
+              </div>
+            </div>
+          ))
+        }
+        <div>
+          {currentPage !== 0 && <button onClick={handlePrev}>Back</button>}
+          {currentPage !== lastPage?.image?.length - 1 && <button onClick={handleNext}>Forward</button>}
+        </div>
+      </>
     </div>
   );
 };
