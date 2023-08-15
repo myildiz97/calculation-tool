da928e3dd59ed66e14fd0a85e0c76adef18cc808
Mehmet Yıldız <myildizwork@gmail.com>
2023-08-15 19:17:51 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,14 +10,14 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
-// app.use(cors({
-//   // origin: "http://localhost:5173",
-//   origin: "https://calculation-tool-client.vercel.app/",
-//   credentials: true,
-//   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
+// app.use(cors());
+app.use(cors({
+  // origin: "http://localhost:5173",
+  origin: "https://calculation-tool-client.vercel.app",
+  credentials: true,
+  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
